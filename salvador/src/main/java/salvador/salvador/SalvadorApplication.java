aaf48bfd2dd62ccc75a509767c8a579f74b97966
ssantgabe2 <gabriel.s@estudante.iftm.edu.br>
2024-08-18 21:20:56 -0300

    sistema juridico

---
+++ salvador/src/main/java/salvador/salvador/SalvadorApplication.java
@@ -0,0 +1,13 @@
+package salvador.salvador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalvadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalvadorApplication.class, args);
+	}
+
+}
---
+++ salvador/src/test/java/salvador/salvador/SalvadorApplicationTests.java
@@ -0,0 +1,13 @@
+package salvador.salvador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalvadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
